e1d6cb7d34c14b7903c8806ebe52a52e07013a28
Potrocio <57481913+Potrocio@users.noreply.github.com>
2025-01-06 19:06:20 -0500

    Update pool.js
    add test to check db connection success

--- db/pool.js
+++ db/pool.js
@@ -5,9 +5,18 @@ const { Pool } = require("pg");
 // All of the following properties should be read from environment variables
 module.exports = new Pool({
   //host: process.env.HOST, // or wherever the db is hosted
-  database_url: process.env.DATABASE_URL,
+  connectionString: process.env.DATABASE_URL,
   user: process.env.USER,
   database: process.env.DATABASE,
   password: process.env.PASSWORD,
   port: process.env.PORT // The default port
 });
+
+(async () => {
+  try {
+    const result = await pool.query('SELECT NOW()');
+    console.log('Connected to database. Current time:', result.rows[0].now);
+  } catch (err) {
+    console.error('Database connection error:', err);
+  }
+})();
